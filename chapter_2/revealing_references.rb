ffe207e4678967e525e87a6f41b3c50564dfd970
skitn <tech.ntaka@gmail.com>
2016-09-05 16:14:29 +0000

    first commit

---
+++ chapter_2/revealing_references.rb
@@ -0,0 +1,24 @@
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel| diameter(wheel)}
+  end
+
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+    Wheel.new(cell[0], cell[1])}
+  end
+end
+
+data = [[1, 2], [3, 4], [5, 6], [7, 8]]
+rr = RevealingReferences.new(data)
+puts rr.diameters
